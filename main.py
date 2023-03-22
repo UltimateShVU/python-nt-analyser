94990ce739166105dc2c860d3bcbfad91a3c3480
root <root@acer>
2023-03-22 08:11:05 +0700

    Добавлены нормальные аргументы запуска Добавлена обработка ошибок при анализе пакетов (в каком-то роде)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from pylibpcap import get_iface_list
 from pylibpcap import get_first_iface
@@ -9,34 +10,51 @@ from sniff import *
 
 import analyze
 from analyze import *
-
-def arg_parser():
-    print(get_iface_list())
-    return
-        
-def main_func(sniffobj):
+ 
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-iL', '--ifacelist', action='store_const', const=True, help='Get list of ifaces and exit') #Get list of ifaces and exit
+    parser.add_argument('iface', nargs='?', help='Custom iface for sniffing') #Custom iface for sniffing
+    parser.add_argument('--nopcap', action='store_const', const=True, help='Don\'t create pcap file') #Don't create pcap file
+    parser.add_argument('-pcap', '--pcap', default='pcap.pcap', help='Custom pcap-file to write to') #Custom pcap-file to write to
+    parser.add_argument('-c', '--count', type=int, default=-1, help='How many packets will be captured. When non provided, keep capturing until program closed') #How many packets should we capture? Default: INF
+    
+    return parser    
+      
+def capt(sniffobj):
     for plen, t, buf in sniffobj.capture():
-        packet = getFriendlyPacket(plen, t, buf)
-        #print(t, "  ", packet['sip'], " ->  ", packet['dip'], " :   ", packet['data'])
-        print(t, packet)
-        
-if "-si" in sys.argv:        
-    arg_parser()
-elif len(sys.argv) > 1:
-    try:
-        sniffobj = getSniffObj(sys.argv[1])
-        main_func(sniffobj)
-    except Exception:
-        print(sys.exc_info())
-else:
-    try:
-        sniffobj = getSniffObj(get_first_iface())
-        main_func(sniffobj)
+        try:
+            packet = getFriendlyPacket(plen, t, buf)
+            #print(t, "  ", packet['sip'], " ->  ", packet['dip'], " :   ", packet['data'])
+            print(t, packet)
+        except Exception:
+            print("\n")
+            print("Error occured while packet analyzing:")
+            print(sys.exc_info())  
+            print("\n")
+            continue
 
-#        stats = sniffobj.stats()
-#        print(stats.capture_cnt, " packets captured")
-#        print(stats.ps_recv, " packets received by filter")
-#        print(stats.ps_drop, "  packets dropped by kernel")
-#        print(stats.ps_ifdrop, "  packets dropped by iface")
+def main_func(iface, of, c):
+    try:
+        sniffobj = Sniff(iface, out_file=of, count=c)
+        capt(sniffobj)
     except Exception:
-        print(sys.exc_info())
+        print(sys.exc_info())   
+
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    args = parser.parse_args()
+    if args.ifacelist:
+        for i in get_iface_list():
+            print(i)
+        sys.exit()
+    iface = args.iface or get_first_iface()
+    pcap = args.pcap
+    if args.nopcap:
+        pcap = "pcap.pcap"
+    count=args.count
+    main_func(iface, pcap, count)
+
+
