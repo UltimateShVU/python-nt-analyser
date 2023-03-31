75ee188f4b65d3d227dbccd5b701e610fa2430ef
root <root@acer>
2023-03-31 11:16:52 +0700

    fix syntax error + remove prints

--- complex.py
+++ complex.py
@@ -5,29 +5,20 @@ keep_running = 1
 def stop_analysis():
     global keep_running
     keep_running = 0
-    print("Signal gone")
 
 def countPackets(packet_queue, result_dict):
-#Protofunction, used for demonstration
-#Counts number of all pacets or dict packets
     i = 0
     j = 0
     while True:
-        print("In count_packets True_cycle")
         global keep_running                           
         if not keep_running:
-            print("Not keep_running, queue size: ", packet_queue.qsize())
-        print("want get queue")
+            print("Count Packets function is now stopping, packets in queue: ", packet_queue.qsize())
         if not packet_queue.empty():
             packet = packet_queue.get()
-            print("got queue")
-            i += 1
             if packet:
                 j += 1
-                print(packet["verdict"], "|", packet["time"], "|", packet["payload"], "|", packet["description"], "|")
         elif not keep_running:
-            print("Gonna return stuff from counPackets")
-            result_dict["Counting packets"] = "Packets accepted: "+str(i)+"  ;   "+"Not 'None' packets accepted: "+str(j)
+            result_dict["Counting packets"] = "Packets accepted: "+str(j)
             return
 
 def complexAnalysis(packets):
@@ -46,15 +37,13 @@ def complexAnalysis(packets):
     
     for packet in packets:
         if not keep_running:        #Stop all threads and print results
-            print("Not keep running in complexAnaysis")
 
             for thread in func_threads:
                 thread.join()
             for data in resulting_data:
-                print(data, "   :   "resulting_data.get(data))
+                print(data, "   :   ", resulting_data.get(data))
             return
         if packet:    
             for q in queues:        #Fill queues
                 a = packet.copy()
-                #print(a)
                 q.put(a)
